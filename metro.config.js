755b5822e094511d7e10e931e5c1d2f89de05460
gabi <70315572+Limgabi@users.noreply.github.com>
2023-10-05 16:24:05 +0900

    chore #2 : icon svg

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,20 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
 
-/**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig();
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+  const svgConfig = {
+    transformer: {
+      babelTransformerPath: require.resolve("react-native-svg-transformer"),
+      assetPlugins: ["react-native-svg-transformer"],
+    },
+    resolver: {
+      assetExts: assetExts.filter((ext) => ext !== "svg"),
+      sourceExts: [...sourceExts, "svg"],
+    },
+  };
+
+  return mergeConfig(getDefaultConfig(__dirname), svgConfig);
+})();
