45f420e3c1f16411fb6b2c052cb10b3a2d4ef303
gabi <70315572+Limgabi@users.noreply.github.com>
2023-10-07 01:12:59 +0900

    fix #2 : header onPress 추가

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,30 @@
-import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import Arrow from "../assets/images/arrow.jpg";
 import { fontStyle } from "../../constants/commonStyle";
 
-const Header = ({ text, back }: { text: string; back?: boolean }) => {
+const Header = ({
+  text,
+  back,
+  onPress,
+}: {
+  text: string;
+  back?: boolean;
+  onPress?: any;
+}) => {
   return (
     <SafeAreaView style={styles.safeAreaContainer}>
       <View style={styles.container}>
         {back && (
-          <View style={styles.iconContainer}>
+          <TouchableOpacity style={styles.iconContainer} onPress={onPress}>
             <Image source={Arrow} style={styles.icon} />
-          </View>
+          </TouchableOpacity>
         )}
         <View style={[styles.textContainer, back && { marginRight: 24 }]}>
           <Text style={fontStyle.Headline3}>{text}</Text>
